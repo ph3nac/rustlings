a00ccd6542d4ef033fdd5500077d9cfa972d7b92
ph3 <100564304+ph3nac@users.noreply.github.com>
2024-08-07 00:43:56 +0900

    COMPLETE: quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: i32) -> i32 {
+    if amount > 40 {
+        1 * amount
+    } else {
+        2 * amount
+    }
+}
 
 // Don't modify this function!
 #[test]
