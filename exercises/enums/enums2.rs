3c63cb0506bf7f54b7c26da60c79b8afca161bc6
ph3 <100564304+ph3nac@users.noreply.github.com>
2024-08-12 22:15:21 +0900

    COMPLETE: enub

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -3,11 +3,12 @@
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
-    // TODO: define the different variants used below
+    Move { x: i32, y: i32 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit, // 紐付けられたデータが存在しないためユニット構造体になる
 }
 
 impl Message {
